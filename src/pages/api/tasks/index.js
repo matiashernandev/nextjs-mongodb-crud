4a30bc0326baf6e94188b77465a4f692e7f8f7c8
nkwaaaa <nkwaaaa@gmail.com>
2023-04-06 11:46:29 -0300

    add get post

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,7 +1,30 @@
-import { dbConnect } from "../../../utils/mongoose";
+import Task from "models/Task";
+import { dbConnect } from "utils/mongoose";
 
 dbConnect();
 
-export default function handler(req, res) {
-	res.status(200).json("tareas");
+export default async function handler(req, res) {
+	const { method, body } = req;
+
+	switch (method) {
+		case "GET":
+			try {
+				const tasks = await Task.find();
+				return res.status(200).json(tasks);
+			} catch (error) {
+				return res.status(400).json({ msg: error.message });
+			}
+		case "POST":
+			try {
+				console.log(body);
+
+				const newTask = new Task(body);
+				const savedTask = await newTask.save();
+				return res.status(201).json(savedTask);
+			} catch (error) {
+				return res.status(400).json({ msg: error.message });
+			}
+		default:
+			return res.status(400).json({ msg: "This method is not supported" });
+	}
 }
